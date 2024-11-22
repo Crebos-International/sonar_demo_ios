61db9e4785ef3ad236690a0fa53bd5ba864b8c3a
Swarajmeet Singh <swarajmeet.crebos@gmail.com>
2024-11-22 15:27:47 +0530

    firstCommit
    firstCommit

---
+++ SonarDemo/SonarDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SonarDemoApp.swift
+//  SonarDemo
+//
+//  Created by Swarajmeet Singh on 21/11/24.
+//
+
+import SwiftUI
+
+@main
+struct SonarDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
